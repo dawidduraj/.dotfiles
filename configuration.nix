b1429cf730c2ba17d5825555ea61926b697bcaef
Dawid Duraj <dawid.duraj@outlook.de>
2025-04-29 21:41:19 +0200

    Add usefull programs

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
+  services.gvfs.enable = true;
   # Enable networking
   networking.networkmanager.enable = true;
   
